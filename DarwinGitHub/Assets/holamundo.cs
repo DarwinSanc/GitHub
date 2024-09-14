47e0d09900574290e677f72700e4e7c163bc993b
DarwinSanc <darwin_asanchez@soy.sena.edu.co>
2024-09-14 09:44:31 -0500

    Primer Script
    agregue un scritp hola mundo

---
+++ DarwinGitHub/Assets/holamundo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class holamundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.log("hola mundo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
